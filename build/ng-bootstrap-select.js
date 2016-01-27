17bf7276e80f87dd964adb22f3ad8edd0c47f6fc
Casey Holzer <casey17648@gmail.com>
2016-01-27 14:17:07 -0700

    release v0.4.0

--- build/ng-bootstrap-select.js
+++ build/ng-bootstrap-select.js
@@ -1,5 +1,5 @@
 /*!
- * ng-bootstrap-select v0.3.1
+ * ng-bootstrap-select v0.4.0
  *
  * Licensed under MIT
  */
@@ -45,15 +45,44 @@ angular.module('ng-bootstrap-select', [])
  * ```
  */
 
-function sortedKeys(obj) {
-  var keys = [];
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      keys.push(key);
-    }
-  }
-  return keys.sort();
+function isWindow(obj) {
+  return obj && obj.window === obj;
 }
+
+var isArray = Array.isArray;
+
+function isArrayLike(obj) {
+
+  // `null`, `undefined` and `window` are not array-like
+  if (obj == null || isWindow(obj)) return false;
+
+  // arrays, strings and jQuery/jqLite objects are array like
+  // * jqLite is either the jQuery or jqLite constructor function
+  // * we have to check the existance of jqLite first as this method is called
+  //   via the forEach method when constructing the jqLite object in the first place
+  if (isArray(obj) || isString(obj) || (jqLite && obj instanceof jqLite)) return true;
+
+  // Support: iOS 8.2 (not reproducible in simulator)
+  // "length" in obj used to prevent JIT error (gh-11508)
+  var length = "length" in Object(obj) && obj.length;
+
+  // NodeList objects (with `item` method) and
+  // other objects with suitable length characteristics are array-like
+  return isNumber(length) &&
+    (length >= 0 && ((length - 1) in obj || obj instanceof Array) || typeof obj.item == 'function');
+
+}
+
+var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?(?:\s+disable\s+when\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/;
+                        // 1: value expression (valueFn)
+                        // 2: label expression (displayFn)
+                        // 3: group by expression (groupByFn)
+                        // 4: disable when expression (disableWhenFn)
+                        // 5: array item variable name
+                        // 6: object item key variable name
+                        // 7: object item value variable name
+                        // 8: collection expression
+                        // 9: track by expression
 
 function selectpickerDirective($parse, $timeout) {
   return {
@@ -63,139 +92,200 @@ function selectpickerDirective($parse, $timeout) {
       var $async = scope.$applyAsync ? '$applyAsync' : '$evalAsync'; // fall back to $evalAsync if using AngularJS v1.2.x
 
       if (attrs.ngOptions) {
-        var NG_OPTIONS_REGEXP = /^\s*([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+group\s+by\s+([\s\S]+?))?\s+for\s+(?:([\$\w][\$\w]*)|(?:\(\s*([\$\w][\$\w]*)\s*,\s*([\$\w][\$\w]*)\s*\)))\s+in\s+([\s\S]+?)(?:\s+track\s+by\s+([\s\S]+?))?$/,
-            multiple = attrs.multiple,
+        var multiple = attrs.multiple,
             optionsExp = attrs.ngOptions,
             optionAttrs = $parse(attrs.bsOptionAttrs)(scope),
-            nullOption,
-            match = optionsExp.match(NG_OPTIONS_REGEXP),
-            displayFn = $parse(match[2] || match[1]),
-            valueName = match[4] || match[6],
-            keyName = match[5],
-            collection = match[7],
-            valuesFn = $parse(collection);
-
-        function bindData(text) {
-          var startIndex,
-              endIndex,
-              index = 0,
-              expressions = [],
-              parseFns = [],
-              textLength = text ? text.length : 0,
-              exp,
-              concat = [],
-              startSymbol = '{{',
-              endSymbol = '}}',
-              startSymbolLength = startSymbol.length,
-              endSymbolLength = endSymbol.length,
-              escapedStartRegexp = new RegExp(startSymbol.replace(/./g, escape), 'g'),
-              escapedEndRegexp = new RegExp(endSymbol.replace(/./g, escape), 'g'),
-              expressionPositions = [];
-              
-          function escape(ch) {
-            return '\\\\\\' + ch;
-          }
+            emptyOption,
+            unknownOption,
+            match = optionsExp.match(NG_OPTIONS_REGEXP);
         
-          function unescapeText(text) {
-            return text.replace(escapedStartRegexp, startSymbol).
-              replace(escapedEndRegexp, endSymbol);
-          }
-              
-          while (index < textLength) {
-            if (((startIndex = text.indexOf(startSymbol, index)) != -1) &&
-                 ((endIndex = text.indexOf(endSymbol, startIndex + startSymbolLength)) != -1)) {
-              if (index !== startIndex) {
-                concat.push(unescapeText(text.substring(index, startIndex)));
-              }
-              exp = text.substring(startIndex + startSymbolLength, endIndex);
-              expressions.push(exp);
-              index = endIndex + endSymbolLength;
-              expressionPositions.push(concat.length);
-              concat.push('');
-            } else {
-              // we did not find an interpolation, so we have to add the remainder to the separators array
-              if (index !== textLength) {
-                concat.push(unescapeText(text.substring(index)));
-              }
-              break;
+        // The collection to watch
+        var collection = match[8];
+        // The variable name for the value of the item in the collection
+        var valueName = match[5] || match[7];
+        // The variable name for the key of the item in the collection
+        var keyName = match[6];
+    
+        // An expression that generates the viewValue for an option if there is a label expression
+        var selectAs = / as /.test(match[0]) && match[1];
+        // An expression that is used to track the id of each object in the options collection
+        var trackBy = match[9];
+        // An expression that generates the viewValue for an option if there is no label expression
+        var valueFn = $parse(match[2] ? match[1] : valueName);
+        var selectAsFn = selectAs && $parse(selectAs);
+        var viewValueFn = selectAsFn || valueFn;
+        var trackByFn = trackBy && $parse(trackBy);
+        
+        // Get the value by which we are going to track the option
+        // if we have a trackFn then use that (passing scope and locals)
+        // otherwise just hash the given viewValue
+        var getTrackByValueFn = trackBy ?
+                                  function(value, locals) { return trackByFn(scope, locals); } :
+                                  function getHashOfValue(value) { return hashKey(value); };
+        var getTrackByValue = function(value, key) {
+          return getTrackByValueFn(value, getLocals(value, key));
+        };
+    
+        var displayFn = $parse(match[2] || match[1]);
+        var groupByFn = $parse(match[3] || '');
+        var disableWhenFn = $parse(match[4] || '');
+        var valuesFn = $parse(collection);
+        
+        var locals = {};
+        var getLocals = keyName ? function(value, key) {
+          locals[keyName] = key;
+          locals[valueName] = value;
+          return locals;
+        } : function(value) {
+          locals[valueName] = value;
+          return locals;
+        };
+            
+        scope.$watch(collection, refresh, true);
+        
+        scope[$async](function () {
+          element.selectpicker($parse(attrs.selectpicker)());
+        });
+      } else {
+        $timeout(function () { // fall back to $timeout for selects that don't use ng-options
+          element.selectpicker($parse(attrs.selectpicker)());
+        });
+      }
+
+      function bindData(text) {
+        var startIndex,
+            endIndex,
+            index = 0,
+            expressions = [],
+            parseFns = [],
+            textLength = text ? text.length : 0,
+            exp,
+            concat = [],
+            startSymbol = '{{',
+            endSymbol = '}}',
+            startSymbolLength = startSymbol.length,
+            endSymbolLength = endSymbol.length,
+            escapedStartRegexp = new RegExp(startSymbol.replace(/./g, escape), 'g'),
+            escapedEndRegexp = new RegExp(endSymbol.replace(/./g, escape), 'g'),
+            expressionPositions = [];
+            
+        function escape(ch) {
+          return '\\\\\\' + ch;
+        }
+      
+        function unescapeText(text) {
+          return text.replace(escapedStartRegexp, startSymbol).
+            replace(escapedEndRegexp, endSymbol);
+        }
+            
+        while (index < textLength) {
+          if (((startIndex = text.indexOf(startSymbol, index)) != -1) &&
+               ((endIndex = text.indexOf(endSymbol, startIndex + startSymbolLength)) != -1)) {
+            if (index !== startIndex) {
+              concat.push(unescapeText(text.substring(index, startIndex)));
             }
+            exp = text.substring(startIndex + startSymbolLength, endIndex);
+            expressions.push(exp);
+            index = endIndex + endSymbolLength;
+            expressionPositions.push(concat.length);
+            concat.push('');
+          } else {
+            // we did not find an interpolation, so we have to add the remainder to the separators array
+            if (index !== textLength) {
+              concat.push(unescapeText(text.substring(index)));
+            }
+            break;
+          }
+        }
+        
+        return function(context, locals) {
+          for (var i = 0, ii = expressions.length; i < ii; i++) {
+            concat[expressionPositions[i]] = $parse(expressions[i])(context, locals);
           }
           
-          return function(context, locals) {
-            for (var i = 0, ii = expressions.length; i < ii; i++) {
-              concat[expressionPositions[i]] = $parse(expressions[i])(context, locals);
+          return concat.join('');
+        }
+      }
+
+      function setAttributes() {
+        if (typeof optionAttrs === 'object' && $.isEmptyObject(optionAttrs)) return;
+        
+        function getOptionValuesKeys(optionValues) {
+          var optionValuesKeys;
+    
+          if (!keyName && isArrayLike(optionValues)) {
+            optionValuesKeys = optionValues;
+          } else {
+            // if object, extract keys, in enumeration order, unsorted
+            optionValuesKeys = [];
+            for (var itemKey in optionValues) {
+              if (optionValues.hasOwnProperty(itemKey) && itemKey.charAt(0) !== '$') {
+                optionValuesKeys.push(itemKey);
+              }
             }
-            
-            return concat.join('');
+          }
+          return optionValuesKeys;
+        }
+        
+        var optionValues = valuesFn(scope) || [],
+            optionValuesKeys = getOptionValuesKeys(optionValues);
+
+        emptyOption = [];
+        unknownOption = [];
+
+        // find empty or unknown options
+        for (var i = 0, children = element.find('option'), ii = children.length; i < ii; i++) {
+          var value = children[i].value,
+              option = children.eq(i);
+
+          if (value === '') {
+            emptyOption.push(option);
+          } else if (value === '?') {
+            unknownOption.push(option);
+          } else {
+            break;
           }
         }
 
-        function setAttributes() {
-          if (typeof optionAttrs === 'object' && $.isEmptyObject(optionAttrs)) return;
+        if (emptyOption.length > 1) emptyOption[1].remove();
 
-          nullOption = false;
+        element.find('option').each(function(i) {
+          if (emptyOption.length) i--;
+          if (unknownOption.length) i--;
 
           var locals = {},
-              getLocals = keyName ? function(value, key) {
-                locals[keyName] = key;
-                locals[valueName] = value;
-                return locals;
-              } : function(value) {
-                locals[valueName] = value;
-                return locals;
-              };
-
-          // find "null" option
-          for (var i = 0, children = element.find('option'), ii = children.length; i < ii; i++) {
-            if (children[i].value === '') {
-              nullOption = children.eq(i);
-              break;
-            }
-          }
-              
-          var values = valuesFn(scope) || [],
-              keys = angular.copy(keyName ? sortedKeys(values) : values);
-              
-          if (!multiple && nullOption) {
-            // insert null option if we have a placeholder, or the model is null
-            keys.unshift(null);
-          }
+              newAttrs = {},
+              newData = {},
+              key = (optionValues === optionValuesKeys) ? i : optionValuesKeys[i],
+              value = optionValues[key];
           
-          element.find('option').each(function(i) {
-            var locals = {},
-                newAttrs = {},
-                newData = {},
-                key = keys[i],
-                value = values[key];
+          locals = getLocals(value, key);
+          
+          if (attrs.bsOptionAttrs && (keyName && key || !keyName && value)) {
+            var customAttrs = typeof optionAttrs === 'function' ? optionAttrs(value, key) : optionAttrs;
             
-            locals = getLocals(value, key);
-            
-            if (key) {
-              var customAttrs = typeof optionAttrs === 'function' ? optionAttrs(key, value) : optionAttrs;
+            if ($.isEmptyObject(customAttrs)) return;
 
-              if ($.isEmptyObject(customAttrs)) return;
-
-              for (var optionAttr in customAttrs) {
-                var attr = customAttrs[optionAttr],
-                    dataAttr = optionAttr.split('data-')[1] ? optionAttr.split('data-')[1].replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); }) : null, // convert to camelCase
-                    parseAttr = bindData(attr)(scope, locals);
-                
-                if (dataAttr) {  
-                  newData[dataAttr] = parseAttr || attr;
-                } else {
-                  newAttrs[optionAttr] = parseAttr || attr;
-                }
+            for (var optionAttr in customAttrs) {
+              var attr = customAttrs[optionAttr],
+                  dataAttr = optionAttr.split('data-')[1] ? optionAttr.split('data-')[1].replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); }) : null, // convert to camelCase
+                  parseAttr = bindData(attr)(scope, locals);
+              
+              if (dataAttr) {  
+                newData[dataAttr] = parseAttr || attr;
+              } else {
+                newAttrs[optionAttr] = parseAttr || attr;
               }
-
-              $(this).data(newData).attr(newAttrs);
             }
-          });
-        }
+
+            $(this).data(newData).attr(newAttrs);
+          }
+        });
       }
 
       function refresh(newVal) {
         scope[$async](function () {
-          if (attrs.bsOptionAttrs) setAttributes();
+          if (attrs.ngOptions && attrs.bsOptionAttrs) setAttributes();
           element.selectpicker('refresh');
         });
       }
@@ -223,14 +313,6 @@ function selectpickerDirective($parse, $timeout) {
           element.selectpicker('setStyle', val);
         });
       });
-
-      scope[$async](function () {
-        element.selectpicker($parse(attrs.selectpicker)());
-      });
-
-      if (optionsExp) {
-        scope.$watch(collection, refresh, true);
-      }
 
       if (attrs.ngModel) {
         scope.$watch(attrs.ngModel, function(newVal, oldVal) {
